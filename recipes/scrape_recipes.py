90b8d7b9d68517e444d439fdc2a06e33c33957b9
Nana Wu <wu.f.nana@gmail.com>
2021-03-20 16:17:48 -0700

    add recipe data

--- recipes/scrape_recipes.py
+++ recipes/scrape_recipes.py
@@ -2,31 +2,52 @@ import csv
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode
 import json
-
-
+import re
+from time import sleep
 
 class RecipeAPIUtils:
 
     def __init__(self):
-        #self.all_dishes = self.get_unique_dishes()
+        self.all_dishes = self.get_unique_dishes()
         self.application_key = 'ea72b2a7589d887c4c93f02157fad0e6'
         self.application_id = '2f11e861'
+        self.recipe_data_dir = 'recipe_data/'
 
-
-    def search_recipe(self, query)->None:
+    def create_dish_filename(self, dish_name)->str:
         """
+        returns a filename with non-alphanumeric characters replaced with _
+        """
+        return self.recipe_data_dir + (re.sub("[^0-9a-zA-Z]+", "_", dish_name)).strip() + '.json'
+
+    def write_recipe(self, dish_name)->None:
+        """
+        Takes the dish name and runs through Edamen's search
         Example GET request: https://api.edamam.com/search?q=chicken&app_id=2f11e861&app_key=ea72b2a7589d887c4c93f02157fad0e6
         Documentation docs: https://developer.edamam.com/edamam-docs-recipe-api
         :param query: recipe query
         """
-        query = urlencode([('q', query), ('app_key', self.application_key), ('app_id', self.application_id)])
-        print(query)
+
+        # replace non-alphanumeric characters with a space
+        clean_dish_name = re.sub("[^0-9a-zA-Z]+", " ", dish_name).strip()
+
+        query = urlencode([('q', clean_dish_name), ('app_key', self.application_key), ('app_id', self.application_id)])
         req = Request('https://api.edamam.com/search?' + query)
-        data = json.load(urlopen(req))
-        print('loaded data')
-        print(data)
+        recipes = json.load(urlopen(req))
+        print('Found {} recipes for "{}" dish'.format(len(recipes['hits']), clean_dish_name))
 
+        dish_filename = self.create_dish_filename(dish_name)
 
+        with open(dish_filename, 'w+') as file:
+            file.write(json.dumps(recipes))
+
+    def write_recipes_for_all_dishes(self)->None:
+        """
+        For all dishes found in menus, find recipes for them
+        """
+        for i, dish_name in enumerate(self.all_dishes):
+            print('Processing dish {}: "{}"'.format(i, dish_name))
+            self.write_recipe(dish_name)
+            sleep(6)
 
     def get_unique_dishes(self)-> list:
         """
@@ -45,4 +66,4 @@ class RecipeAPIUtils:
 
 if __name__ == '__main__':
     api = RecipeAPIUtils()
-    api.search_recipe('toast')
+    api.write_recipes_for_all_dishes()
