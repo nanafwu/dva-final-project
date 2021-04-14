530a2de955841a491378a7fc307ab00edc2aab5a
Darius Tran <dariustran@gmail.com>
2021-04-14 16:00:29 -0700

    Updated code to generate data to be used during d3 visualization. Added header names to restaurant_list.

--- display_dishes.py
+++ display_dishes.py
@@ -9,6 +9,7 @@ class UserInput(object):
   def __init__(self):
     # dataframe that contains entire recipe database with the similar matches
     self.recipe_df = None
+    self.restaurants_df = None
     # dataframe
     self.matches_df = []
     self.matched_dish = None
@@ -19,8 +20,14 @@ class UserInput(object):
     Read in CSV file and store in class member
     '''
     self.recipe_df = pd.read_csv(filepath, index_col=0)
+    # print(self.recipe_df.columns.values)
     # df.iloc[:,np.r_[0:2,4:5]]
     return
+
+  def read_restaurant_tsv(self, filepath):
+    self.restaurants_df = pd.read_table(filepath)
+    # print(self.restaurants_df)
+    # print(self.restaurants_df.columns.values) # file header
 
   def find_best_match(self, search_str):
     '''
@@ -51,7 +58,7 @@ class UserInput(object):
       self.matched_dish = self.matches_df.iloc[0]
 
       # Index of the similar dishes converted to int and placed into a list.
-      self.similar_dishes = [int(x) for x in self.matches_df.iloc[0]['Dishes_in_Cluster'][1:-1].split(',')]
+      self.similar_dishes = [int(x) for x in self.matched_dish['Top_5_Indices'][1:-1].split(',')]
 
     return len(self.matches_df)
 
@@ -59,19 +66,68 @@ class UserInput(object):
   def display_dishes(self):
     print(self.recipe_df.iloc[self.similar_dishes]['Dish_Name'].to_string(index=False))
 
+  def generate_csv_for_d3(self):
+    '''
+    DISH_ID, DISH_NAME, SIM_DISH_ID1,...,SIM_DISH_ID5, SIM_DISH_NAME_1,...,SIM_DISH_NAME5,REST_NAME1,...,REST_NAME5
+    '''
+
+    data = []
+    
+
+    # restaurant url (Restaurant_URL):
+    # /ny/new-york/375922-america-s-finest-deli/menu/
+  
+    # print(self.restaurants_df.loc[:, ['Restaurant_Name']])
+
+    for row in self.recipe_df.itertuples():
+      # print(row.Top_5_Indices)
+      dish_rest_ids = [int(x) for x in row.Top_5_Indices[1:-1].split(',')]
+      dish_rest_names = [x.replace("'", "") for x in row.Top_5_Dish_Names[1:-1].split(', ')]
+      new_row = [row.Index,row.Dish_Name]
+      new_row.append(dish_rest_ids)
+      new_row.append(dish_rest_names)
+
+      rest_names = []
+      for id in dish_rest_ids:
+        restaurant_url = self.recipe_df.iloc[id]['Restaurant_URL']
+        # print(restaurant_url)
+        restaurant_name = self.restaurants_df.loc[self.restaurants_df['Restaurant_URL'].str.contains(restaurant_url)]['Restaurant_Name'].to_string(index=False).strip()
+        # print(restaurant_name)
+
+        rest_names.append(restaurant_name)
+      
+      new_row.append(rest_names)
+      # print(new_row)
+      data.append(new_row)
+      np_arr = np.array(data, dtype=object)
+    # print(np_arr.shape)
+
+    # Create pandas df
+    df = pd.DataFrame(np_arr)
+    # print(df)
+    headers = ['Index', 'Dish_Name', 'Similar_Index', 'Similar_Dishes', 'Location_of_Similar']
+    df.columns = ['Index', 'Dish_Name', 'Similar_Index', 'Similar_Dishes', 'Location_of_Similar']
+    df.to_csv('d3_data.csv', index=False, columns=headers)
+
+
+
+    
+
 if __name__ == "__main__":
   ui = UserInput()
   ui.read_recipes_csv('Similar_Recipes_man_sub.csv')
-  while(True):
-    try:
-      search_recipe = str(input("Enter your favorite dish: "))
-      if(search_recipe == '\n' or search_recipe == "" or search_recipe == " "):
-        print("Please enter a valid recipe")
-      else:
-        num_results = ui.find_best_match(search_recipe)
-        if(num_results > 0):
-          ui.display_dishes()
-        else:
-          print("No matches found")
-    except Exception as e:
-      print(e)
+  ui.read_restaurant_tsv('./menu/menu_data/restaurant_list.tsv')
+  ui.generate_csv_for_d3()
+  # while(True):
+  #   try:
+  #     search_recipe = str(input("Enter your favorite dish: "))
+  #     if(search_recipe == '\n' or search_recipe == "" or search_recipe == " "):
+  #       print("Please enter a valid recipe")
+  #     else:
+  #       num_results = ui.find_best_match(search_recipe)
+  #       if(num_results > 0):
+  #         ui.display_dishes()
+  #       else:
+  #         print("No matches found")
+  #   except Exception as e:
+  #     print(e)
